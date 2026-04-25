cc42925c84810fdad22e8fd8179c6a416c0abda8
Mark_Mannion <markj.mannion@icloud.com>
2026-04-25 19:02:49 +0200

    Added MV logo to the taskbar and window title bar

--- main.js
+++ main.js
@@ -15,17 +15,22 @@ let doctorName = null
 let patientId = null
 
 function createWindow() {
+    // Define icon path
+    const iconPath = path.join(__dirname, 'taskbar_logo.jpeg')
+
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        icon: iconPath,
         webPreferences: {
-        preload: path.join(__dirname, 'preload.js'),
-        contextIsolation: true,
-        nodeIntegration: false
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true,
+            nodeIntegration: false
         }
     })
+
     Menu.setApplicationMenu(null)
-  mainWindow.loadFile('index.html')
+    mainWindow.loadFile('index.html')
 }
 
 app.whenReady().then(createWindow)
